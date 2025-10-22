9777f3259b624540a23d358f9c200aa818c326d8
Jordy <jordyslagter@icloud.com>
2025-10-22 10:59:53 +0200

    add a visualisation container

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const svgWidth = 500;
 const svgHeight = 400;
 
 const svg = d3
-  .select("body")
+  .select("#visualisation-container")
   .append("svg")
   .attr("viewBox", `0 0 ${svgWidth} ${svgHeight}`)
   .attr("width", svgWidth)
